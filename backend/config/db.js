322622cd614a86d3781d2c982b66b2610d258915
Akshay Ashok Bauskar <116937702+AKSHU4254@users.noreply.github.com>
2024-09-06 16:08:09 +0530

    Update db.js
    DB config Changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://akshaybauskar47:Akshu4254@cluster0.oif2dio.mongodb.net/food-del').then(()=>console.log("DataBase Connected"))
+    await mongoose.connect('mongodb+srv://@cluster0.oif2dio.mongodb.net/food-del').then(()=>console.log("DataBase Connected"))
 }
