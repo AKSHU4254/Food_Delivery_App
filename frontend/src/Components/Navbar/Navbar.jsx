8a87d340b2408f3ebc5fd45ecdd1cdfe149c657c
Akshay Bauskar <akshaybauskar47@gmail.com>
2024-09-18 09:31:10 +0530

    Navbar token

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = ({ setShowLogin }) => {
     const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
     const navigate = useNavigate();
     const logout = () => {
-        localStorage.removeItem("Token");
+        localStorage.removeItem("token");
         setToken("");
         navigate('/')
     }
